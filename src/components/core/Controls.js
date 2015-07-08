3b95530655375a377b8ac62bb528f4b60ecc5b68
Daniel Mahal <danielmahal@gmail.com>
2015-07-08 05:31:50 +0200

    Simple controls

--- src/components/core/Controls.js
+++ src/components/core/Controls.js
@@ -1,20 +1,68 @@
 import React from 'react'
 
-export default class Controls extends React.Component {
+export default class ControlGroup extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      expanded: true
+    }
+  }
+
+  toggleExpanded() {
+    this.setState({
+      expanded: !this.state.expanded
+    })
+  }
+
+  onChange(e) {
+    this.props.onChange(JSON.parse(e.currentTarget.value))
+  }
+
   render() {
+    const {expanded} = this.state
+
     const style = {
       position: 'absolute',
       top: 0,
       left: 0,
+      right: this.state.expanded ? 0 : 'auto',
+      bottom: this.state.expanded ? 0 : 'auto',
+      display: 'flex',
+      flexDirection: 'column',
       fontFamily: 'monospace',
-      background: 'black',
-      color: 'white',
-      WebkitUserSelect: 'none'
+      background: 'rgba(0, 0, 0, 0.5)',
+      color: 'white'
+    }
+
+    const headerStyle = {
+      cursor: 'pointer',
+      padding: '1em'
+    }
+
+    const textareaStyle = {
+      fontFamily: 'inherit',
+      background: 'transparent',
+      color: 'inherit',
+      border: '0',
+      outline: 'none',
+      flex: 1,
+      resize: 'none',
+      padding: '1em'
     }
 
     return (
       <div style={style}>
-        {this.props.children}
+        <header style={headerStyle} onClick={this.toggleExpanded.bind(this)}>
+          {this.state.expanded ? 'Close controls' : 'Open controls'}
+        </header>
+
+        { this.state.expanded &&
+          <textarea
+            style={textareaStyle}
+            value={JSON.stringify(this.props.value, null, '  ')}
+            onChange={this.onChange.bind(this)} />
+        }
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,53 +1,34 @@
 import React from 'react'
 
 import Controls from './components/core/Controls'
-import ControlGroup from './components/core/ControlGroup'
-import SliderControl from './components/core/SliderControl'
 import Layer from './components/core/Layer'
 
-import PointGrid from './components/PointGrid'
-import CrossGrid from './components/CrossGrid'
-
-class Invert extends Layer {
-  draw(context) {
-    context.save()
-    context.globalCompositeOperation = 'xor'
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-    context.restore()
-  }
-}
+import Invert from './components/Invert'
 
 class Application extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      gridSize: 60
+      config: {
+        grid: {
+          size: 120
+        }
+      }
     }
   }
 
-  onConfigChange(key, value) {
-    this.setState({
-      [key]: value
-    })
+  onConfigChange(config) {
+    this.setState({config})
   }
 
   render() {
     return (
       <div>
-        <Controls>
-          <ControlGroup label="Grid">
-            <SliderControl label="Size" min={20} max={200} step={1} defaultValue={this.state.gridSize} onChange={this.onConfigChange.bind(this, 'gridSize')} />
-          </ControlGroup>
-        </Controls>
+        <Controls value={this.state.config} onChange={this.onConfigChange.bind(this)} />
 
         <Layer>
           <Invert>
-            <Layer debug>
-              <PointGrid key="full" size={this.state.gridSize} pointSize={1} />
-              <PointGrid key="half" size={this.state.gridSize / 3} pointSize={0.5} />
-              <CrossGrid key="cross" size={this.state.gridSize} min={1} max={10} />
-            </Layer>
           </Invert>
         </Layer>
       </div>
